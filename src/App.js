221bd8104ab905d7422a93808c31391ccbb399a2
Edgar Posada <edgarpsda@Edgars-MacBook-Air.local>
2022-05-31 12:29:34 -0700

    Add cards with testimonials

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Testimonial from "./components/Testimonial";
+const data = require("./data.json");
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenedor-principal">
+        <h1>Esto es lo que dicen nuestros alumnos sobre Freecodecamp:</h1>
+        {data.map((element, index) => {
+          return (
+            <Testimonial
+              key={index}
+              nombre={element.nombre}
+              pais={element.pais}
+              imagen={element.imagen}
+              cargo={element.cargo}
+              empresa={element.empresa}
+              testimonio={element.testimonio}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
